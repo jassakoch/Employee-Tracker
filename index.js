eea77a51726b2569efab3583225baa0b9691d387
paulina koch <pjassakoch@gmail.com>
2024-01-04 19:48:17 -0500

    Added function to view department, role and employee tables

--- index.js
+++ index.js
@@ -29,11 +29,10 @@ async function promptManager() {
       }
     ])
     console.log(answers);
-};
-promptManager();
+
 
 //Switch statement to handle diffent menu options
-switch (task) {
+switch (answers.menu) {
   case "View all departments":
     viewDepartmentTable();
     break;
@@ -45,32 +44,68 @@ switch (task) {
   case "View all employees":
     viewEmployeeTable();
     break;
+}
+//   case "Add a department":
+//     addDepartment();
+//     break;
 
-  case "Add a department":
-    addDepartment();
-    break;
+//   case "Add a role":
+//     updateEmployeeRole();
+//     break;
 
-  case "Add a role":
-    updateEmployeeRole();
-    break;
+//   case "Add an employee role":
+//     addEmployeeRole();
+//     break;
 
-  case "Add an employee role":
-    addEmployeeRole();
-    break;
-
-  case "End":
-    connection.end();
-    break;
-};
+//   case "End":
+//     connection.end();
+//     break;
+// };
 
 //Function to view the department table
 function viewDepartmentTable() {
   console.log("Viewing department table\n");
-  let departmentTable = 
-  `SELECT * FROM DEPARTMENT`
-  return departmentTable;
-
+  db.query(`SELECT * FROM department`, (err, result) => {
+    if (err) throw err
+    console.table (result)
+    promptManager();
+  });
 }
+
+//Function to view role table
+function viewRoleTable() {
+  console.log("Viewing Employee Role Table\n");
+  db.query(`select role.id,role.title,role.salary,department.name from role join department on role.department_id=department.id`, (err, result) => {
+    if (err) throw err
+    console.table (result)
+    promptManager();
+  });
+}
+
+//Function to view employees table
+function viewEmployeeTable() {
+  console.log("Viewing Employee Role Table\n");
+  db.query(`SELECT 
+  employee.id AS ID,
+  employee.first_name AS \`First Name\`, 
+  employee.last_name AS \`Last Name\`, 
+  role.title AS Title, 
+  role.salary AS Salary, 
+  department.name AS Department,
+  CONCAT(manager.first_name, ' ', manager.last_name) AS Manager
+FROM employee 
+JOIN role ON employee.role_id = role.id
+LEFT JOIN department ON role.department_id = department.id
+LEFT JOIN employee AS manager ON employee.manager_id = manager.id
+ORDER BY employee.id`, (err, result) => {
+    if (err) throw err
+    console.table (result)
+    promptManager();
+  });
+}
+
+};
+promptManager();
 
 //view all  roles select role.id,role.title,role.salary,department.name from role join department on role.department_id=department.id;
 
@@ -79,18 +114,17 @@ function viewDepartmentTable() {
 //same query to view managers of employees but changing the column title of manager last name: select employee.id, employee.first_name, employee.last_name, role.title, manager.last_name as manager from employee join role on employee.role_id=role.id left join employee as manager on employee.manager_id=manager.id;
 
 //viewing all employees : 
-/*
-SELECT 
-                        employee.id AS ID,
-                        employee.first_name AS \`First Name\`, 
-                        employee.last_name AS \`Last Name\`, 
-                        role.title AS Title, 
-                        role.salary AS Salary, 
-                        department.name AS Department,
-                        CONCAT(manager.first_name, ' ', manager.last_name) AS Manager
-                    FROM employee 
-                    JOIN role ON employee.role_id = role.id
-                    LEFT JOIN department ON role.department_id = department.id
-                    LEFT JOIN employee AS manager ON employee.manager_id = manager.id
-                    ORDER BY employee.id`
-                    */
+
+// SELECT 
+//                         employee.id AS ID,
+//                         employee.first_name AS \`First Name\`, 
+//                         employee.last_name AS \`Last Name\`, 
+//                         role.title AS Title, 
+//                         role.salary AS Salary, 
+//                         department.name AS Department,
+//                         CONCAT(manager.first_name, ' ', manager.last_name) AS Manager
+//                     FROM employee 
+//                     JOIN role ON employee.role_id = role.id
+//                     LEFT JOIN department ON role.department_id = department.id
+//                     LEFT JOIN employee AS manager ON employee.manager_id = manager.id
+//                     ORDER BY employee.id`
